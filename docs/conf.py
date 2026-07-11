f61ed612289bdf0834a755790e1646d27cab7189
pcmaticds <kjhgdhu@outlook.com>
2026-07-11 15:46:41 +0530

    Add Sphinx configuration for documentation
    Initialize Sphinx documentation configuration for the project.

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = 'How-to-Recover-Pc-Matic-Account'
+author = 'How-to-Recover-Pc-Matic-Account'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
